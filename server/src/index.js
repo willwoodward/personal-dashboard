95455cd623fdf83103ff69a51973154995ad43c1
willwoodward <will@woodwardweb.com>
2025-08-13 21:51:03 +0100

    initialised server

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+const port = "3000";
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    console.log("Response sent");
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
